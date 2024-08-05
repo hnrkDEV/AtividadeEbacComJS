be1b2fca5100b27011d9384f2bea66662db14a78
João Henrique <jhenrique.caval@hotmail.com>
2024-08-05 18:09:36 -0300

    Realizado novas alterações na atividade

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
-const form = document.getElementById('form-deposito');
+const form = document.getElementById('form-deposito'); 
+const nomeBeneficiario = document.getElementById("nome-beneficiario")
+let formEvalido = false;
 function validaNome(nomeCompleto){
-    const nomeComoArray = nomeCompleto.slipt(" ");
+    const nomeComoArray = nomeCompleto.split(" ");
     return nomeComoArray.length >= 2;
+} // Função para validar o nome
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+       const accountNumero = document.getElementById("conta-deposito")
+       const valorQuantia = document.getElementById("valor-deposito")
+       const descricao = document.getElementById("descricao")
+       const mensagemSucesso = `Sucesso! <br>O montante de: <b>R$${valorQuantia.value}</b> foi enviado para:<br>
+       <b>Cliente:</b>
+       ${nomeBeneficiario.value}<br>
+       <b>conta:</b> ${accountNumero.value}<br>
+       <b>Descrição do depósito:</b><br> 
+       ${descricao.value}`
+
+       formEvalido = validaNome(nomeBeneficiario.value) 
+       if(formEvalido) {
+        const containerMensagemSucesso = document.querySelector('.sucess-message')
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = "block"
+        document.querySelector(".message-error").style.display = "none"
+        nomeBeneficiario.style.border = "none";
+    //mensagem de sucesso ^
 }
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    let usuario = {
-        nome: document.getElementById("nome-beneficiario").value ,
-        agencia: document.getElementById("agencia-deposito").value,
-        account: document.getElementById("conta-deposito").value,
-        valor: document.getElementById("valor-deposito").value,
-        descricao: document.getElementById("descricao").value
+    else {
+        nomeBeneficiario.style.border = "1px solid red";
+        document.querySelector(".message-error").style.display = "block"
+        containerMensagemSucesso.style.display = "none"
     }
-    document.getElementById("sucess-message").innerHTML = `Sucesso! <br>O montante de: <b>R$${document.getElementById("valor-deposito").value}
-    </b> foi enviado para:<br><b>Cliente:</b>
-    ${document.getElementById("nome-beneficiario").value}<br><b>conta:</b> ${document.getElementById("conta-deposito").value}
-    <br><b>Descrição do depósito:</b> <br> ${document.getElementById("descricao").value}`         
-    console.log(usuario)
-    document.getElementById('form-deposito').innerHTML += ""
 })
 
+nomeBeneficiario.addEventListener('keyup', function(e) {
+    console.log(e.target.value);
+    formEvalido = validaNome(e.target.value) 
 
+    if(!formEvalido) {
+        nomeBeneficiario.classList.add('error')
+        nomeBeneficiario.style.border = "1px solid red";
+        document.querySelector(".message-error").style.display = "block"
+    }
+    else {
+        nomeBeneficiario.classList.remove('error')
+        nomeBeneficiario.style = " ";
+        document.querySelector(".message-error").style.display = "none"
+    }
+})
+ 
